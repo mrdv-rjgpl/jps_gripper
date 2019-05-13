528ab7fd6515a54db3d4f727817d2d659de5d908
Mardava Gubbi <mgubbi1@jhu.edu>
2019-05-13 13:46:01 -0400

    Add gripper test

--- src/test_gripper.cpp
+++ src/test_gripper.cpp
@@ -10,28 +10,6 @@
 std_msgs::UInt16 servo_angle;
 ros::Publisher Servo_pub;
 
-void Sub_Callback(const sensor_msgs::Joy::ConstPtr& msg)
-{
-  ROS_INFO("Subscriber callback triggered");
-
-  int servo_status[2];
-  servo_status[0] = msg->buttons[0];
-  servo_status[1] = msg->buttons[1];
-  // std::cout<<"JOY IS"<<kk<<std::endl;
-
-  if (servo_status[0] == 1)
-  {
-    servo_angle.data = 150;
-    Servo_pub.publish(servo_angle);
-  }
-
-  if (servo_status[1] == 1)
-  {
-    servo_angle.data = 0;
-    Servo_pub.publish(servo_angle);
-  }
-}
-
 int main(int argc, char** argv)
 {
   ros::init(argc, argv, "joy_servo");
@@ -39,7 +17,16 @@ int main(int argc, char** argv)
 
   // defining the subscriber, publisher and the timer objects
   Servo_pub = node.advertise<std_msgs::UInt16>("/servo", 1);
-  ros::Subscriber Sub_joy = node.subscribe("/joy", 100, Sub_Callback);
+  servo_angle.data = 0;
+
+  for(int i = 0; i < 10; ++i)
+  {
+    servo_angle.data = 150 - servo_angle.data;
+    ROS_INFO_STREAM("Publishing servo command with angle " << servo_angle.data << "...");
+    Servo_pub.publish(servo_angle);
+    ros::Duration(5.0).sleep();
+  }
+
   ros::spin();
 
   return 0;
