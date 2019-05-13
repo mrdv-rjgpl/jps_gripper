f6152057634ec41eee0f4f9b1e51362532d6959f
Mardava Gubbi <mgubbi1@jhu.edu>
2019-05-13 13:26:36 -0400

    Indent and tweak node names

--- src/joy_servo.cpp
+++ src/joy_servo.cpp
@@ -7,47 +7,40 @@
 #include <std_msgs/UInt16.h>
 #include <iostream>
 
-
 std_msgs::UInt16 servo_angle;
 ros::Publisher Servo_pub;
 
-void Sub_Callback(const sensor_msgs::Joy::ConstPtr &msg)
+void Sub_Callback(const sensor_msgs::Joy::ConstPtr& msg)
 {
-
-
   // ROS_INFO("Callback 1000222222 triggered");
 
   int servo_status[2];
   servo_status[0] = msg->buttons[0];
   servo_status[1] = msg->buttons[1];
-  //std::cout<<"JOY IS"<<kk<<std::endl;
+  // std::cout<<"JOY IS"<<kk<<std::endl;
 
-   
-      if(servo_status[0]==1){
-      servo_angle.data = 150; 
-      Servo_pub.publish(servo_angle);
-      }
+  if (servo_status[0] == 1)
+  {
+    servo_angle.data = 150;
+    Servo_pub.publish(servo_angle);
+  }
 
-      if(servo_status[1]==1){
-      servo_angle.data = 0; 
-      Servo_pub.publish(servo_angle);
-      }
-
+  if (servo_status[1] == 1)
+  {
+    servo_angle.data = 0;
+    Servo_pub.publish(servo_angle);
+  }
 }
 
-
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-
   ros::init(argc, argv, "joy_servo");
   ros::NodeHandle node;
 
-// defining the subscriber, publisher and the timer objects
+  // defining the subscriber, publisher and the timer objects
   Servo_pub = node.advertise<std_msgs::UInt16>("/servo", 1);
   ros::Subscriber Sub_joy = node.subscribe("/joy", 100, Sub_Callback);
   ros::spin();
 
   return 0;
 }
-
-
